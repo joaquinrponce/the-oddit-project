05abe0228fb11c991480e4495e127d0212b421bc
Kokozord <koko_lacteos666@hotmail.com>
2020-06-18 15:44:41 -0500

    sidebar file

---
+++ client/src/mainSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default class MainSidebar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    return null
+  }
+}
