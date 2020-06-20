fbe64e28663615f19317c96dbc6a275a36a9dd38
Kokozord <koko_lacteos666@hotmail.com>
2020-06-19 22:07:31 -0500

    use fetch for knock key

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -1,11 +1,8 @@
 Knock.setup do |config|
 
   # Point knock to secret
-  if !Rails.env.production?
     config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
-  else
-    config.token_secret_signature_key = ENV["SECRET_KEY_BASE"]
-  end
+
 
   ## Expiration claim
   ## ----------------
