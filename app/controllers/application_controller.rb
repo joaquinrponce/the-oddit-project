27a4f527fe45a1188a98ca4d8414a334d668510e
Kokozord <koko_lacteos666@hotmail.com>
2020-06-20 15:35:12 -0500

    try to fix rails redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::API
   include Knock::Authenticable
 
   def fallback_index_html
-    render :file => 'public/client/index.html'
+    render :file => 'client/build/index.html'
   end
 end
