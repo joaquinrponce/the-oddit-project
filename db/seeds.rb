b1d2885495f6c45eb61b34fc64d805f2c798ba75
Kokozord <koko_lacteos666@hotmail.com>
2020-06-19 21:14:26 -0500

    fix seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,51 +6,52 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+valhalla = Hall.create(name: 'valhalla')
+theodinproject =  Hall.create(name: 'theodinproject')
+music = Hall.create(name: 'music')
+gaming = Hall.create(name: 'gaming')
+technology = Hall.create(name: 'technology')
 
-first = User.create(name: 'Koko', password: 'test', password_confirmation: 'test', role: 3)
-second = User.create(name: 'Kokozordo', password: 'testo', password_confirmation: 'testo')
-users = [first, second]
+if !Rails.env.production?
+  first = User.create(name: 'Koko', password: 'test', password_confirmation: 'test', role: 3)
+  second = User.create(name: 'Kokozordo', password: 'testo', password_confirmation: 'testo')
+  users = [first, second]
 
-valhalla = Hall.create(name: 'Valhalla')
-bifrost =  Hall.create(name: 'Bifrost')
-folkvangr = Hall.create(name: 'Folkvangr')
-himinbjorg = Hall.create(name: 'Himinbjorg')
-bilskirnir = Hall.create(name: 'Bilskirnir')
-
-halls = [valhalla, bifrost, folkvangr, himinbjorg, bilskirnir]
-
-content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+  halls = [valhalla, theodinproject, music, gaming, technology]
+  
+  content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
           laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
           voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
           non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-titles = [
+          titles = [
     'William Shakespeare', 'Emily Dickinson', 'Edgar Allan Poe', 'Walt Whitman',
     'E. E. Cummings', 'William Wordsworth', 'Henry Wadsworth Longfellow',
     'T. S. Eliot', 'Emily Bronte', 'Oscar Wilde', 'Rudyard Kipling',
     'Charlotte Bronte', 'Geoffrey Chaucer', 'Charles Baudelaire',
     'Ralph Waldo Emerson'
-]
+  ]
 
 
-2.times do
-  user = users.sample
-  halls.each do |hall|
-    post = user.posts.create(title: titles.sample, body: content, hall: hall)
+  2.times do
+    user = users.sample
+    halls.each do |hall|
+      post = user.posts.create(title: titles.sample, body: content, hall: hall)
+    end
   end
-end
 
-2.times do
-  user = users.sample
-  Post.all.each do |post|
-    post.comments.create(body: "Test comment!", user: user)
+  2.times do
+    user = users.sample
+    Post.all.each do |post|
+      post.comments.create(body: "Test comment!", user: user)
+    end
   end
-end
 
-3.times do
-  user = users.sample
-  Comment.all.each do |comment|
-    comment.replies.create(body: "Test reply!", user: user)
+  3.times do
+    user = users.sample
+    Comment.all.each do |comment|
+      comment.replies.create(body: "Test reply!", user: user)
+    end
   end
 end
