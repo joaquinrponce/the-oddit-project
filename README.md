3c9ac90527d1db1e0bb02ce20a1d00e857008e01
Kokozord <koko_lacteos666@hotmail.com>
2020-07-06 19:51:19 -0500

    rollback react-bootstrap to 1.0.1

--- README.md
+++ README.md
@@ -1,24 +1,19 @@
-# README
+# THE ODDIT PROJECT
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+A Reddit clone made with Rails API + React, as the final project for the
+JavaScript course in The Odin Project.
 
-Things you may want to cover:
+Most of Reddit's important features are implemented, such as:
 
-* Ruby version
+- Creating Halls (subreddits)
+- Creating posts as Text-only, image-only or URL-only
+- Commenting, with nested comments
+- Adding and removing moderators to a hall
+- Customizing hall sidebars
+- Subscribing to a hall, which populates a user's /feed
+- Voting and downvoting posts and comments
+- User profiles, which display info about that user
 
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+There's more things I would like to implement, but as of current release this is as far
+as I want to go with this project. Doing this was a great learning experience, and I'm sure
+spending more time would teach me even more, but I'd like to move on to something different.
--- client/src/navigation/userControls.js
+++ client/src/navigation/userControls.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Nav } from 'react-bootstrap'
+import { Nav, Dropdown } from 'react-bootstrap'
 import {userContext} from '../userContext.js'
 import PostModal from '../halls/posts/postModal.js'
 import UserModal from './user/userModal.js'
@@ -32,7 +32,39 @@ export default class UserControls extends React.Component {
       { this.context.loggedIn &&
           <Nav.Link as='button' onClick={this.togglePostModal}>+ New Post</Nav.Link>
       }
-      
+      { this.context.loggedIn &&
+        <Dropdown>
+          <Dropdown.Toggle as={Nav.Link}>
+            {this.context.user.name}
+            </Dropdown.Toggle>
+          <Dropdown.Menu alignRight id='user-actions-dropdown' title={this.context.user.name}>
+            <Dropdown.Item as={Link} to={`/users/${this.context.user.name}`}>
+              My Profile
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => {this.toggleUserModal('password')}}>
+              Change Password
+            </Dropdown.Item>
+            <Dropdown.Item as={Link} to='/logout' onClick={this.context.logOutUser}>
+              Logout
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      }
+      { !this.context.loggedIn &&
+        <Dropdown>
+          <Dropdown.Toggle as={Nav.Link}>
+            {this.context.user.name}
+            </Dropdown.Toggle>
+          <Dropdown.Menu alignRight id='user-actions-dropdown' title='Guest'>
+            <Dropdown.Item onClick={() => {this.toggleUserModal('login')}}>
+              Login
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => {this.toggleUserModal('signup')}}>
+              Sign Up
+              </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      }
         <UserModal show={this.state.showUserModal} type={this.state.type} hideModal={this.toggleUserModal}/>
         <PostModal show={this.state.showPostModal} hideModal={this.togglePostModal}/>
       </Nav>
